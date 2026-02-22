d04952e4b770dbe05f14676de05f4fd64b271e06
RajeshDangi2312 <59009788+RajeshDangi2312@users.noreply.github.com>
2026-02-22 12:53:29 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -12,19 +12,19 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 class Config(object):
 
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1801402133:REPLACE_WITH_YOUR_ACTUAL_TOKEN")
+
+    # Get from my.telegram.org (Must be an integer)
+    APP_ID = int(os.environ.get("APP_ID", 36118940))
 
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
-
-    # Get from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "06770b3e3bb635f3c1e4ec733b46dfbf")
 
     # Authorized users to use this bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1801402133").split())
 
     # Generate a user session string
-    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
+    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "groupcloner_session")
 
     # Database URI
     DB_URI = os.environ.get("DATABASE_URL", "")
